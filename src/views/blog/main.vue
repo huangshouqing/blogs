0b11d3009a758856fc9f87f30183f5da9401c80c
huangshouqing <2830474591@qq.com>
2021-06-19 21:13:53 +0800

    feat: 修改看板娘

--- src/views/blog/main.vue
+++ src/views/blog/main.vue
@@ -16,24 +16,22 @@ export default {
   },
   methods: {},
   created() {
-    setTimeout(function () {
-      L2Dwidget.init({
-        model: {
-          jsonPath:
-            "https://unpkg.com/live2d-widget-model-koharu@1.0.5/assets/koharu.model.json",
-          scale: 1,
-        },
-        display: {
-          position: "right",
-          width: 140,
-          height: 300,
-          hOffset: 0,
-          vOffset: -20,
-        },
-        mobile: { show: true, scale: 0.5 },
-        react: { opacityDefault: 0.7, opacityOnHover: 0.2 },
-      });
-    }, 1000);
+    L2Dwidget.init({
+      model: {
+        jsonPath:
+          "https://unpkg.com/live2d-widget-model-koharu@1.0.5/assets/koharu.model.json",
+        scale: 1,
+      },
+      display: {
+        position: "right",
+        width: 140,
+        height: 300,
+        hOffset: 0,
+        vOffset: -20,
+      },
+      mobile: { show: true, scale: 0.5 },
+      react: { opacityDefault: 0.7, opacityOnHover: 0.2 },
+    });
   },
 };
 </script>
